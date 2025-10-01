ef69163c20d31b591d1e73be49677b22cad019ec
emancilla01 <emancilla01@gmail.com>
2025-09-30 21:07:28 -0500

    created a new inclass practice to fix bootstrap and vue compatibility

---
+++ resources/views/plantillas/plantilla1.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/js/app2.ts'])
+</head>
+<body>
+    
+
+
+    <input type="button" value="hola" class="btn btn-primary">
+    <button>prueba</button>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/prueba', function () {
+    return view('inicio');
+});
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return view("inicio_autententicado"); //Inertia::render('Dashboard');
+})->middleware(['auth', 'verified']); //->name('dashboard');
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
